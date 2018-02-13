06cd15c187b270c31be2b51b208010ebb45172ed
Evgen <Akei@i.ua>
2018-02-13 17:32:45 +0200

    3

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express =require('express');
 var winston = require('winston');
 var fs = require("fs");
+var sql = require('mssql');
 
 var nodeEnv = process.env.NODE_ENV;
 const app= express();
 
 var list=['вася','оля', 'игорь', 'валера', 'я'];
 var listJson = JSON.stringify(list);
+
+// config for your database
+var config = {
+    user: 'sa',
+    password: '33421pop',
+    server: 'localhost',
+    database: 'GMSData38',
+    options: {
+        encrypt: true
+    }
+};
+sql.connect(config, function (err) {
+    console.log("connect to GMSData38");
+    if (err) console.log(err); });
+
+// connect to your database
+// sql.connect(config, function (err) {
+//     console.log("connect to GMSData38");
+//     if (err) console.log(err);
+//     var request = new sql.Request();
+//     request.query('select ChID,ProdID,ProdName,UM,Country,Notes,PGrID,Article1 from r_Prods WHERE ChID<20;', function (err, recordset) {
+//         if (err) console.log(err);
+//         console.log(recordset);
+//
+//     });
+// });
 
 var loggerTransports= [];
 if(nodeEnv=='development')
@@ -42,6 +69,24 @@ app.get('/list', function (req,res) {
     if(!list2) list2=JSON.parse(fs.readFileSync(__dirname+"/name.json", "utf8"));
     list2.push("asdf");
     res.send(JSON.stringify(list2));
+});
+var nameString;
+app.get('/2/getList',function (req,res) {
+    if(!nameString) nameString=JSON.parse(fs.readFileSync(__dirname+"/name.json", "utf8"));
+    nameString
+        .push("Name");
+    res.send(nameString);
+});
+
+app.get('/2/getListFromDb',function (req,res) {
+
+    var request = new sql.Request();
+    request.query('select ChID,ProdID,ProdName,UM from r_Prods WHERE ChID<20;', function (err, recordset) {
+        if (err) console.log(err);
+        res.send(recordset.recordset);
+    });
+
+
 });
 
 app.listen(3000, function(){
